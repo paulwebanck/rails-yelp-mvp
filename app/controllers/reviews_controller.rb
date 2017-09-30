9b1f27cb7f64e249c2499fcae91d0c389bc9589e
Paul Webanck <paul.webanck@edhec.com>
2017-09-30 17:22:02 +0200

    rake is all green

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,8 +9,11 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     # we need `restaurant_id` to asssociate review with corresponding restaurant
     @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    if @review.save
     redirect_to restaurant_path(@review.restaurant)
+    else
+    render :new
+  end
   end
 
   private
