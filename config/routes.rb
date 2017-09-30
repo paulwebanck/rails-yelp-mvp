bea91a882930b9531e6caa8c5aace8303e73da30
Paul Webanck <paul.webanck@edhec.com>
2017-09-30 16:23:03 +0200

    app is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [ :show]
 end
+
+
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show"
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
